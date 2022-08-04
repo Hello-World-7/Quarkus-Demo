5a30dfab632e6b69eaaa311660def2e3274b1a9c
Sattar Rahimbayli <sattar.rahimbayli@dedalus.eu>
2022-08-04 23:16:12 +0200

    Dummy project

---
+++ create.sql
@@ -0,0 +1,1 @@
+create table Book (id integer not null, name varchar(255), year integer not null, primary key (id)) engine=InnoDB;
---
+++ run.sh
@@ -0,0 +1,1 @@
+mvn clean install quarkus:dev -Dquarkus.http.host=0.0.0.0
